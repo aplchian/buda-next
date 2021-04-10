1efbbdb970676e81abd67965b97e3d7af86766ad
aplchian <alex.boquis@gmail.com>
2021-04-10 15:23:42 -0400

    show placeholder auth img

--- components/AppBar.tsx
+++ components/AppBar.tsx
@@ -47,8 +47,8 @@ function AppBarTwo(props: {
                                         <div>
                                             <button type="button" className="max-w-xs bg-indigo-600 rounded-full flex items-center text-sm text-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-indigo-600 focus:ring-white" id="user-menu" aria-expanded="false" aria-haspopup="true">
                                                 <span className="sr-only">Open user menu</span>
-                                                {/*<img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixqx=p2TOxaXI3E&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />*/}
-                                                <AuthNavBar/>
+                                                <img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixqx=p2TOxaXI3E&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+                                                {/*<AuthNavBar/>*/}
                                             </button>
                                         </div>
                                         {/*
@@ -106,8 +106,8 @@ function AppBarTwo(props: {
                         <div className="pt-4 pb-3 border-t border-indigo-700">
                             <div className="flex items-center px-5">
                                 <div className="flex-shrink-0">
-                                    {/*<img className="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixqx=p2TOxaXI3E&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />*/}
-                                    <AuthNavBar/>
+                                    <img className="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixqx=p2TOxaXI3E&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+                                    {/*<AuthNavBar/>*/}
                                 </div>
                                 <div className="ml-3">
                                     <div className="text-base font-medium text-black">Tom Cook</div>
